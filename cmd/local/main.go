648f7055d21359dbe73024efcf99fe9ad9cf6289
anh_tuan.l <anh_tuan.l@kbtg.tech>
2023-03-15 16:39:51 +0700

    Hello world

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/ServiceWeaver/weaver"
 )
 
 func main() {
-	root := weaver.Init(context.Background())
+	url := "localhost:12345"
 
-	root.New()
+	root := weaver.Init(context.Background())
+	opts := weaver.ListenerOptions{
+		LocalAddress: url,
+	}
+
+	lis, err := root.Listener("hello", opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("hello listener available on %v\n", lis)
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!\n", r.URL.Query().Get("name"))
+	})
+	http.Serve(lis, nil)
 }
